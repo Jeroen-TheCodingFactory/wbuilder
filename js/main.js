daebd1b7253a5dff366c5074ac5d25a58160e1d5
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2023-08-23 15:22:55 +0200

    refactored

--- js/main.js
+++ js/main.js
@@ -1,58 +1,27 @@
-fetch("../data/cardsStructure.json").then(response => response.json()).then(data => render(data));
-
-function renderCards(data){
-    for(let i =0; i < data.length; i++){
-        let article = document.createElement("article");
-        article.classList += "card";
-
-        let header = document.createElement("header");
-        header.classList = "card__header";
-        article.appendChild(header);
-
-        let heading = document.createElement("h1");
-        heading.classList = "card__heading";
-        heading.innerText = data[i].title;
-        header.appendChild(heading);
-
-        let div = document.createElement("div");
-        article.appendChild(div);
-
-        let section = document.createElement("section");
-        section.classList = "card__body";
-        div.appendChild(section);
-
-        let text = document.createElement("p");
-        text.innerText = data[i].text;
-        section.appendChild(text);
-
-        let footer = document.createElement("footer");
-        footer.classList = "card__footer";
-        div.appendChild(footer);
-
-        let button = document.createElement("button");
-        button.classList = "card__action";
-        button.innerText = data[i].action;
-        footer.appendChild(button);
-
-        document.querySelector("body").appendChild(article);
-    }
+function getData(url) {
+    fetch(url).then(response => response.json()).then(data => render(data));
 }
 
-function render(data,parent){
+function render(data, parent) {
     let childElement;
-    for(let i = 0; i < data.length; i++){
-        childElement = document.createElement(data[i].tag);
-        childElement.classList = data[i].classList;
-        if(data[i].root === true){
+    data.forEach(element => {
+        childElement = document.createElement(element.tag);
+        childElement.classList = element.classList;
+        if (element.root === true) {
             parent = document.querySelector("body");
         }
-
-        if(data[i].text !== undefined){
-            childElement.innerText = data[i].text;
+        if (element.text !== undefined) {
+            childElement.innerText = element.text;
         }
-        if(data[i].children.length > 0){
-            render(data[i].children, childElement);
+        if (element.children.length > 0) {
+            render(element.children, childElement);
         }
         parent.appendChild(childElement);
-    }
+    })
 }
+
+function init() {
+    getData("../data/data.json");
+}
+
+init();
